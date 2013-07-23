14a4bb02c74c55e2cb4d5f154b0e9ba9f84d0998
Azat Mardanov <azat.mardanov@gmail.com>
2013-07-22 17:06:36 -0700

    Update db.js
    add cross-environment example

--- db/db.js
+++ db/db.js
@@ -18,3 +18,25 @@ db.open(function(e,c){
 	db.close();
 });
 
+//cross-environment script
+/*
+var url = require('url')
+var util = require('util');
+var mongodb = require ('mongodb');
+var Db = mongodb.Db;
+var Connection = mongodb.Connection;
+var Server = mongodb.Server;
+
+var dbConnUrl = process.env.MONGOHQ_URL ||
+'mongodb://127.0.0.1:27017';
+var host = url.parse(dbConnUrl).hostname;
+var port = new Number(url.parse(dbConnUrl).port);
+
+ 
+var db=new Db ('test', new Server(host,port, {}));
+db.open(function(e,c){
+  // console.log (util.inspect(db));
+  console.log(db._state);
+  db.close();
+});
+*/
