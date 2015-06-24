e0fb5f79ba01b4f632b9f96ffc13262afba9055a
Azat Mardan <hi@azat.co>
2015-06-23 20:56:58 -0700

    make react work witout db

--- board-react/js/app.js
+++ board-react/js/app.js
@@ -43,5 +43,131 @@ var Footer = React.createClass({
   }
 });
 
+var MessageList = React.createClass({
+  displayName: "MessageList",
+
+  render: function render() {
+    var messages = this.props.messages;
+    console.log(messages);
+    if (!messages.length > 0) return React.createElement(
+      "tr",
+      null,
+      React.createElement(
+        "td",
+        { colspan: "2" },
+        "No messages yet"
+      )
+    );
+    return React.createElement(
+      "div",
+      { className: "span12" },
+      React.createElement(
+        "table",
+        { className: "table table-bordered table-striped" },
+        React.createElement(
+          "caption",
+          null,
+          "Chat"
+        ),
+        React.createElement(
+          "thead",
+          null,
+          React.createElement(
+            "tr",
+            null,
+            React.createElement(
+              "th",
+              { className: "span2" },
+              "Username"
+            ),
+            React.createElement(
+              "th",
+              null,
+              "Message"
+            )
+          )
+        ),
+        React.createElement(
+          "tbody",
+          null,
+          messages.map(function (message) {
+            return React.createElement(
+              "tr",
+              { key: message.id },
+              React.createElement(
+                "td",
+                null,
+                message.username
+              ),
+              React.createElement(
+                "td",
+                null,
+                message.message
+              )
+            );
+          })
+        )
+      )
+    );
+  }
+});
+
+var NewMessage = React.createClass({
+  displayName: "NewMessage",
+
+  addMessage: function addMessage() {
+    this.props.addMessageCb({
+      username: React.findDOMNode(this.refs.username).value,
+      message: React.findDOMNode(this.refs.message).value
+    });
+    React.findDOMNode(this.refs.username).value = "";
+    React.findDOMNode(this.refs.message).value = "";
+  },
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "row-fluid", id: "new-message" },
+      React.createElement(
+        "div",
+        { className: "span12" },
+        React.createElement(
+          "form",
+          { className: "well form-inline" },
+          React.createElement("input", { type: "text", name: "username", className: "input-small", placeholder: "Username", ref: "username" }),
+          React.createElement("input", { type: "text", name: "message", className: "input-small", placeholder: "Message Text", ref: "message" }),
+          React.createElement(
+            "a",
+            { id: "send", className: "btn btn-primary", onClick: this.addMessage },
+            "SEND"
+          )
+        )
+      )
+    );
+  }
+});
+
+var MessageBoard = React.createClass({
+  displayName: "MessageBoard",
+
+  getInitialState: function getInitialState() {
+    return { messages: [{ id: 1, username: "Azat", message: "hi" }] };
+  },
+  addMessage: function addMessage(message) {
+    var messages = this.state.messages;
+    message.id = Math.random();
+    messages.push(message);
+    this.setState({ messages: messages });
+  },
+  render: function render() {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(MessageList, { messages: this.state.messages }),
+      React.createElement(NewMessage, { messages: this.state.messages, addMessageCb: this.addMessage })
+    );
+  }
+});
+
 React.render(React.createElement(Header, null), document.getElementById("header"));
 React.render(React.createElement(Footer, null), document.getElementById("footer"));
+React.render(React.createElement(MessageBoard, null), document.getElementById("message-board"));
--- board-react/js/app.jsx
+++ board-react/js/app.jsx
@@ -21,5 +21,87 @@ var Footer = React.createClass({
   }
 })
 
+
+var MessageList = React.createClass({
+  render: function(){
+    var messages = this.props.messages
+    console.log(messages)
+    if (!messages.length>0) return (
+      <tr>
+        <td colspan="2">No messages yet</td>
+      </tr>
+    )
+    return (
+      <div className="span12">
+        <table className="table table-bordered table-striped">
+          <caption>Chat</caption>
+          <thead>
+            <tr>
+              <th className="span2">Username</th>
+              <th>Message</th>
+            </tr>
+          </thead>
+          <tbody>
+            {messages.map(function(message){
+              return (
+                <tr key={message.id}>
+                  <td>{message.username}</td>
+                  <td>{message.message}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+})
+
+
+var NewMessage = React.createClass({
+  addMessage: function(){
+    this.props.addMessageCb({
+      username: React.findDOMNode(this.refs.username).value,
+      message: React.findDOMNode(this.refs.message).value
+    })
+    React.findDOMNode(this.refs.username).value = ''
+    React.findDOMNode(this.refs.message).value = ''
+  },
+  render: function(){
+    return (
+      <div className="row-fluid" id="new-message">
+        <div className="span12">
+          <form className="well form-inline" >
+            <input type="text" name="username" className="input-small" placeholder="Username" ref="username"/>
+            <input type="text" name="message" className="input-small" placeholder="Message Text" ref="message" />
+            <a id="send" className="btn btn-primary" onClick={this.addMessage}>SEND</a>
+          </form>
+        </div>
+      </div>
+    )
+  }
+})
+
+var MessageBoard = React.createClass({
+  getInitialState: function(){
+    return {messages: [{id: 1, username: 'Azat', message: 'hi'}]}
+  },
+  addMessage: function(message){
+    var messages = this.state.messages
+    message.id = Math.random()
+    messages.push(message)
+    this.setState({messages: messages})
+  },
+  render: function(){
+    return (
+      <div>
+        <MessageList messages={this.state.messages} />
+        <NewMessage messages={this.state.messages} addMessageCb={this.addMessage} />
+      </div>
+    )
+  }
+})
+
 React.render(<Header />, document.getElementById('header'))
 React.render(<Footer />, document.getElementById('footer'))
+React.render(<MessageBoard />, document.getElementById('message-board'))
