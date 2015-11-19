a576f4cc4f9857ea303b6f9ddff1b2228c67e339
Azat Mardan <hi@azat.co>
2015-11-19 13:41:01 -0800

    changes

--- rest/js/app.js
+++ rest/js/app.js
@@ -1,5 +1,5 @@
-var parseID="your-parse-app-id";
-var parseKey="your-rest-api-key";
+var parseID="Zc36GIp6WyzKIB9HvqRBEGnIeMO0X21rDbVwGPvp";
+var parseKey="1LKZVd4KG6PFpldvCha5RDY8Z5EZeJhy2Bl4vgJ3";
 
 $(document).ready(function(){
 	getMessages();
@@ -33,7 +33,7 @@ $(document).ready(function(){
 })
 function getMessages() {
 	$.ajax({
-		url: " https://api.parse.com/1/classes/MessageBoard",
+		url: " https://api.parse.com/1/classes/MessageBoard?limit=1000",
 		headers: {
 			"X-Parse-Application-Id": parseID,
 			"X-Parse-REST-API-Key": parseKey
@@ -51,12 +51,13 @@ function getMessages() {
 	});
 }
 
-function updateView(messages) {	
+function updateView(messages) {
+	// messages.results = messages.results.reverse()
 	var table=$(".table tbody");
 	table.html('');
 	$.each(messages.results, function (index, value) {
-		var trEl=$('<tr><td>'+value.username+'</td><td>'+value.message+'</td></tr>');		
-		table.append(trEl);		
+		var trEl=$('<tr><td>'+value.username+'</td><td>'+value.message+'</td></tr>');
+		table.append(trEl);
 	});
 
 	console.log(messages);
