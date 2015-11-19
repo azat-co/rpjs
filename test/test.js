14b59d074e92f2eb0ee11ef575dd78880287dcb6
Azat Mardan <hi@azat.co>
2015-11-19 14:06:05 -0800

    fix #8

--- test/test.js
+++ test/test.js
@@ -8,4 +8,4 @@ var messageBoard = require('./mb-server');
 
 assert.deepEqual('[{"name":"John","message":"hi"}]', messageBoard.getMessages());
 assert.deepEqual('{"name":"Jake","message":"gogo"}',messageBoard.addMessage("name=Jake&message=gogo"));
-assert.deepEqual('[{"name":"John","message":"hi"},{"name":"Jake","message":"gogo"}]', messageBoard.getMessages("name=Jake&message=gogo"));
+assert.deepEqual('[{"name":"John","message":"hi"},{"name":"Jake","message":"gogo"}]', messageBoard.getMessages());
